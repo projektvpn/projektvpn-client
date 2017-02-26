27a26f2aa6a5c70fd7aa9ddc0258ad84c023b59c
ProjektVPN <projektvpn@protonmail.com>
2017-02-25 20:29:15 -0800

    Add a basic framework that opens and closes and minimizes to tray

---
+++ app/index.js
@@ -0,0 +1,54 @@
+// index.js: runs in the background page and sometimes opens windows.
+
+// Note that any uncaught errors in this context will kill the background page.
+// You need to have a window open so that you can "inspect background page" and
+// see the console.
+
+console.log('Background page started.')
+
+// We'll set this with our one window if/when it's wanted
+var win = null;
+var tray = null;
+
+// Open up a window that's not visible.
+// The window code can do all our actual VPN client work.
+nw.Window.open('index.html', {show: false}, function(new_win) {
+  // Remember the window
+  win = new_win
+  win.on('close', function() {
+    this.hide() // Close to tray
+  })
+})
+
+// Create the UI if not created, and display it otherwise
+function show_ui() {
+  if (win) {
+    // Show the window we have
+    win.show()
+  }
+}
+
+// Quit the application
+function quit() {
+  if (win) {
+    win.hide()
+  }
+  tray.remove()
+  process.exit()
+}
+
+tray = new nw.Tray({ icon: 'img/trayicon.png', iconsAreTemplates: false })
+tray.tooltip = 'ProjektVPN'
+// Show the UI when the icon is left-clicked
+tray.on('click', function() {
+  show_ui()
+})
+
+// Set up system tray menu
+var menu = new nw.Menu();
+menu.append(new nw.MenuItem({ label: 'Show UI', click: show_ui }))
+menu.append(new nw.MenuItem({ label: 'Quit', click: quit }))
+tray.menu = menu
+
+
+
